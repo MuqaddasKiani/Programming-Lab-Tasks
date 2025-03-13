7599415cc5726ceffb4c639be10a07c7955dfe89
MuqaddasKiani <kianimuqaddass@gmail.com>
2025-03-13 20:51:45 +0500

    update

--- task8.cpp
+++ task8.cpp
@@ -112,30 +112,30 @@ int main()
 
 //                                    TASK4
 
-//   double usd, converted;
-//    int choice;
-//    cout << "Enter amount in USD: ";
-//    cin >> usd;
-//    cout << "Choose the currency to convert to (1 for PKR, 2 for INR, 3 for Euros): ";
-//    cin >> choice;
-//    switch (choice) 
-//	{
-//        case 1:
-//            converted = usd * 280;
-//            cout << "Converted amount in PKR: " << converted << endl;
-//            break;
-//        case 2:
-//            converted = usd * 80;
-//            cout << "Converted amount in INR: " << converted << endl;
-//            break;
-//        case 3:
-//            converted = usd * 0.9; 
-//            cout << "Converted amount in Euros: " << converted << endl;
-//            break;
-//        default:
-//            cout << "Error: Invalid currency!" << endl;
-//    }
-//    return 0;
+    double usd, converted;
+    int choice;
+    cout << "Enter amount in USD: ";
+    cin >> usd;
+    cout << "Choose the currency to convert to (1 for PKR, 2 for INR, 3 for Euros): ";
+    cin >> choice;
+    switch (choice) 
+	{
+        case 1:
+            converted = usd * 280;
+            cout << "Converted amount in PKR: " << converted << endl;
+            break;
+        case 2:
+            converted = usd * 87.1;
+            cout << "Converted amount in INR: " << converted << endl;
+            break;
+        case 3:
+            converted = usd * 0.93; 
+            cout << "Converted amount in Euros: " << converted << endl;
+            break;
+        default:
+            cout << "Error: Invalid currency!" << endl;
+    }
+    return 0;
 
 //                          TASK5
 
